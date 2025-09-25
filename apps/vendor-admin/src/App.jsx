d8f02706839f239c20553bcc60f481f4bfc4efe5
Ragul-JustLogix <ragul.m@justlogix.com>
2025-09-25 14:26:42 +0530

    kjkjksdlds

---
+++ apps/vendor-admin/src/App.jsx
@@ -0,0 +1,27 @@
+
+import React from "react";
+import { Button } from "ui";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>vendor Admin Dashboard </h1>
+      <h1>Hii admin </h1>
+
+      <Button
+        variant="primary"
+        size="large"
+        onClick={() => alert("Super Admin Action!")}
+      >
+        Create New User
+      </Button>
+      <br />
+      <br />
+      <Button variant="danger" onClick={() => confirm("Delete all data?")}>
+        Delete All
+      </Button>
+    </div>
+  );
+}
+
+export default App;
